fed40f90c632b0591048cb208f0d5ff4e65ab29b
Betina9 <ob042025@gmail.com>
2025-04-28 12:51:33 +0200

    test 6

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-favouriteMovieGenre("romantic");
+// Challenge 2:
+// Replace the arguments below according to your preference.
+
+// space, scary, military, romantic, cowboy, fantasy, superhero
+favouriteMovieGenre("fantasy");
 
 // watermelon, tomato, banana, orange, avocado, blueberry
 favouriteFruit("blueberry");
 
 // light, dark
-favouriteMode("light");
+favouriteMode("dark");
 
 // sharp, soft, round
-favouriteEdgeStyle("soft");
-
-function setProp(prop, value) {
-  document.documentElement.style.setProperty(prop, value);
-}
-
-function favouriteEdgeStyle(style) {
-  setProp("--image", "var(--" + style + ")");
-}
-
-function favouriteMovieGenre(font) {
-  if (font) {
-    setProp("--font", "var(--" + font + ")");
-  }
-}
+favouriteEdgeStyle("round");
 
 function favouriteMode(mode) {
   if (mode === "light" || !mode) {
